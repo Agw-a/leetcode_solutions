e3b4ca0af3a665804ad5f3812fbdffcb45f3324d
A-gwa <89134520+Agw-a@users.noreply.github.com>
2023-01-29 20:19:38 +0300

    Time: 130 ms (40.60%), Space: 42.3 MB (68.60%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+        let n = nums.length
+    let indices = []
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] === target) {
+                indices.push(i, j)
+            }
+        }
+    }
+    return indices
+};
