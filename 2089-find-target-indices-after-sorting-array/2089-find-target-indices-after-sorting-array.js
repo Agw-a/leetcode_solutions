5f6802698f4d8d4e6d3ee83947c701fcd7790b5a
A-gwa <89134520+Agw-a@users.noreply.github.com>
2023-02-22 22:58:14 +0300

    Time: 70 ms (65.70%), Space: 43.7 MB (19.31%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    nums = nums.sort((a, b) => a - b)
+    let res = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            res.push(i)
+        }
+        
+    }
+    return res
+};
